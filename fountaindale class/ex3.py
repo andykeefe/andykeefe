5107c1a2dfcb03ff66567d28efe03471450cc92a
andykeefe <154836099+andykeefe@users.noreply.github.com>
2023-12-26 12:52:03 -0600

    Update ex3.py

--- fountaindale class/ex3.py
+++ fountaindale class/ex3.py
@@ -3,8 +3,8 @@ b = "Cherish your youth"
 
 while True:  # start a loop
 
-    print("How old are you?")
-    age = int(input())    # str type --> int type
+    print("How old are you?")  # input() reads whatever data is put in as a str type
+    age = int(input())    # Type conversion: str type --> int type
     
     if age >= 60:  # condition 1
         print(a)
