8afab3dd7c2e07aea7119c29749a46a80ddcc40f
andykeefe <154836099+andykeefe@users.noreply.github.com>
2023-12-26 12:48:17 -0600

    commented ex2.py

--- fountaindale class/ex2.py
+++ fountaindale class/ex2.py
@@ -5,4 +5,8 @@ print(a)
 print(a, b)
 print(a + b)		
 print(a*b)
-print(type(a))
+
+print(type(a))  # A function type() inside of a function print()
+
+# print(type()) prints the type of variable in the type() function
+# print(type(250)) would output <class 'int'> , print(type("aaa")) would output <class 'str'>
